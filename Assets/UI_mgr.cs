5c4829da66d86f940bd4c195775359f0f185f256
Alex Morais <thatsamorais@mail.com>
2012-12-01 23:50:02 -0700

    Initial commit of WebRP with initial assets

---
+++ Assets/UI_mgr.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class UI_mgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		GUILayout.BeginArea();
+		
+		GUILayout.EndArea();
+	}
+	
+	void GUIButtonRect() {
+		
+	}
+}
