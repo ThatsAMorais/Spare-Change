8a7426f6fb922e3bd7aa467262e55edc15268f3b
Alex Morais <thatsamorais@mail.com>
2012-12-02 23:07:54 -0700

    At this point the dice are being thrown from the camera at a reasonable time scale, with no loading delay, randomly colored.  All collisions seem to be correct.

--- Assets/UI_mgr.cs
+++ Assets/UI_mgr.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class UI_mgr : MonoBehaviour {
 	
+	static private IList<string> diceStr = new List<string>();
+	
 	public GameObject d4;
 	public GameObject d6_s_p;
 	public GameObject d6_s_d;
@@ -16,19 +18,19 @@ public class UI_mgr : MonoBehaviour {
 	public GameObject d100;
 	public GameObject cube;
 	public float throwForce;
+	public List<Material> DiceMats;
 	
 	private int dieCount = 0;
-	//private bool throw_a_dice = false;
-	
-	static private IList<string> diceStr = new List<string>();
+	//private bool throw_a_dice = false;	
 	
 	// Use this for initialization
 	void Start () {
 		diceStr.Add("d4");
 		diceStr.Add("d6_s_p");
 		diceStr.Add("d6_s_d");
-		diceStr.Add("d6_r_p");
-		diceStr.Add("d6_r_d");
+		/*These are impossible to work with due to their poly count*/
+		/*diceStr.Add("d6_r_p");
+		diceStr.Add("d6_r_d");*/
 		diceStr.Add("d8");
 		diceStr.Add("d10");
 		diceStr.Add("d12");
@@ -45,16 +47,15 @@ public class UI_mgr : MonoBehaviour {
 	{
 		float force_x = throwForce; //throwForce * Input.GetAxis("Vertical");
 		float force_z = throwForce; //throwForce * Input.GetAxis("Horizontal");
-		Debug.Log(string.Format("Force_x({0}) Force_y({1})", force_x, force_z));
 		
-		die.rigidbody.AddForce(force_x, 0.0f, -1 * force_z);
-		die.rigidbody.AddTorque(2.0f, 4.0f, 2.0f);
+		die.rigidbody.AddForce(0, 0.0f, force_z);
+		die.rigidbody.AddTorque(600.0f, 600.0f, 600.0f);
 	}
 	
 	void CreateDie(string die) {
 		
 		GameObject newDie;
-		//Vector3 camPos = Camera.main.transform.position;
+		Vector3 camPos = Camera.main.transform.position;
 		
 		if(die.Equals("d4")){
 			newDie = Instantiate(d4) as GameObject;
@@ -91,22 +92,23 @@ public class UI_mgr : MonoBehaviour {
 		}
 		
 		newDie.name = string.Format("{0}_{1}", newDie.name, dieCount);
-		newDie.transform.position = new Vector3(0, 12.0f, 0);
+		newDie.transform.position = camPos;
+		
+		// Give them a random texture
+		newDie.renderer.materials = new Material[2] {DiceMats[Random.Range(0, DiceMats.Count)], DiceMats[Random.Range(0, DiceMats.Count)]};
 		
 		dieCount++;
-		
-		Debug.Log(newDie.name);		
 		ThrowDie(newDie);
 	}
 	
 	Rect GUIButtonRect() {
-		return new Rect(Screen.width*0.01f, Screen.height*0.01f, Screen.width*0.98f, Screen.height*0.2f);
+		return new Rect(Screen.width*0.01f, Screen.height*0.01f, Screen.width*0.98f, Screen.height*0.4f);
 	}
 	
 	void OnGUI() {
 		GUILayout.BeginArea(GUIButtonRect());
-		GUILayout.BeginHorizontal();
 		
+		GUILayout.BeginHorizontal();
 		foreach(string die in diceStr)
 		{
 			if(GUILayout.Button(die))
@@ -114,8 +116,14 @@ public class UI_mgr : MonoBehaviour {
 				CreateDie(die);
 			}
 		}
-		
 		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace();
+		GUILayout.TextField(string.Format("Dice Created: {0}", dieCount));
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+		
 		GUILayout.EndArea();
 	}
 }
